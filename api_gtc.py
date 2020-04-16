1494787eb73046a53edc2ba8cba0330994b0aeba
Hakim Moumen <hakim.moumen@gmail.com>
2020-04-16 09:41:14 +0200

    Modification API

--- api_gtc.py
+++ api_gtc.py
@@ -13,14 +13,27 @@ app = Flask(__name__)
 
 items = []
 
-gtc.init()
+gtc1 = gtc.gtc()
+relay1 = gtc1.relay(25)
+relay2 = gtc1.relay(8)
+relay3 = gtc1.relay(5)
+relay4 = gtc1.relay(6)
+ultrasonic1 = gtc1.ultrasonic(14, 15)
+rain1 = gtc1.rain(17)
+rain2 = gtc1.rain(27)
 
-@app.route('/api/gtc', methods=['GET'])
+@app.route('/api/gtc_status', methods=['GET'])
 def get_average_depth():
-    result = gtc.average_depth()
-    print (result)
+    result = {"Depth":[], "Relais":[], "Pluviometre":[]}
+    result["Depth"].append(ultrasonic1.average_depth())
+    result["Relais"].append(relay1.status())
+    result["Relais"].append(relay2.status())
+    result["Relais"].append(relay3.status())
+    result["Relais"].append(relay4.status())
+    result["Pluviometre"].append(rain1.status())
+    result["Pluviometre"].append(rain2.status())
     return jsonify({'item': result}), 201
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host= '0.0.0.0', debug=True)
--- gtc.py
+++ gtc.py
@@ -5,69 +5,58 @@ from datetime import datetime
 import sys
 import datetime
 
-class relay:
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+tank_min = 130
+well_min = 250
+watering_hour = "02:43 PM"
 
-	def __init__(self, gpio):
-		self.gpio = gpio
-		print('Initialisation du relais GPIO : {}'.format(self.gpio))
-		GPIO.setup(self.gpio, GPIO.OUT)
-		self.on()
-		time.sleep(0.5)
-		self.off()
-		time.sleep(0.5)
-
-	def on(self):
-		GPIO.output(self.gpio, GPIO.HIGH)
-	def off(self):
-		GPIO.output(self.gpio, GPIO.LOW)
-	def status(self):
-		if (GPIO.input(self.gpio)==1):
-			print('Status GPIO {} : UP'.format(self.gpio))
-		else:
-			print('Status GPIO {} : DOWN'.format(self.gpio))
-
-
-class ultrasonic:
-
-	def __init__(self, gpio_trigger, gpio_echo):
-		self.gpio_trigger = gpio_trigger
-		self.gpio_echo = gpio_echo
-		print('Initialisation du capteur ultrason GPIO : ')
-		print('\t Trigger GPIO \t: {}'.format(self.gpio_trigger))
-		print('\t Echo GPIO \t: {}'.format(self.gpio_echo))
-		GPIO.setup(self.gpio_trigger,GPIO.OUT)
-		GPIO.setup(self.gpio_echo,GPIO.IN)
-		depth_per = 100 - ((self.average_depth() / 150) * 100)
-		print("Reservoir rempli à {} %".format(int(depth_per)))
+class gtc:
+	def __init__(self):
+		print('Initialisation de la Gestion Technique centralisée')
 		time.sleep(0.5)
 
-	def instant_depth(self):
-		GPIO.output(self.gpio_trigger, GPIO.LOW)
-		time.sleep(0.5)
-		GPIO.output(self.gpio_trigger, GPIO.HIGH) # set TRIGGER to HIGH
-		time.sleep(0.00001) # wait 10 microseconds
-		GPIO.output(self.gpio_trigger, GPIO.LOW) # set TRIGGER to LOW
-		start = time.time()
-		while GPIO.input(self.gpio_echo)==0:
-			start = time.time()
-		# Assign the actual time to stop variable until the ECHO goes back from HIGH to LOW
-		while GPIO.input(GPIO_ECHO)==1:
-			stop = time.time()
-		# Calculate the time it took the wave to travel there and back
-		measuredTime = stop - start
-		# Calculate the travel distance by multiplying the measured time by speed of sound
-		distanceBothWays = measuredTime * 33112 # cm/s in 20 degrees Celsius
-		# Divide the distance by 2 to get the actual distance from sensor to obstacle
-		depth = distanceBothWays / 2
-		return depth
+	def cleanup(self):
+		GPIO.cleanup()
 
-	def average_depth(self):
-		i = 0
-		depth_sum = 0
-		depth = 0
-		GPIO.output(self.gpio_trigger, GPIO.LOW)
-		time.sleep(0.5)
-		while (i < 10):
+	class relay:
+
+		def __init__(self, gpio):
+			self.gpio = gpio
+			print('Initialisation du relais GPIO : {}'.format(self.gpio))
+			GPIO.setup(self.gpio, GPIO.OUT)
+			self.on()
+			time.sleep(0.5)
+			self.off()
+			time.sleep(0.5)
+			self.type = "relay"
+
+		def on(self):
+			GPIO.output(self.gpio, GPIO.HIGH)
+		
+		def off(self):
+			GPIO.output(self.gpio, GPIO.LOW)
+		
+		def status(self):
+			return GPIO.input(self.gpio)
+
+	class ultrasonic:
+
+		def __init__(self, gpio_trigger, gpio_echo):
+			self.gpio_trigger = gpio_trigger
+			self.gpio_echo = gpio_echo
+			print('Initialisation du capteur ultrason GPIO : ')
+			print('\t Trigger GPIO \t: {}'.format(self.gpio_trigger))
+			print('\t Echo GPIO \t: {}'.format(self.gpio_echo))
+			GPIO.setup(self.gpio_trigger,GPIO.OUT)
+			GPIO.setup(self.gpio_echo,GPIO.IN)
+			depth_per = 100 - ((self.average_depth() / 150) * 100)
+			print("Reservoir rempli à {} %".format(int(depth_per)))
+			time.sleep(0.5)
+
+		def instant_depth(self):
+			GPIO.output(self.gpio_trigger, GPIO.LOW)
+			time.sleep(0.5)
 			GPIO.output(self.gpio_trigger, GPIO.HIGH) # set TRIGGER to HIGH
 			time.sleep(0.00001) # wait 10 microseconds
 			GPIO.output(self.gpio_trigger, GPIO.LOW) # set TRIGGER to LOW
@@ -75,7 +64,7 @@ class ultrasonic:
 			while GPIO.input(self.gpio_echo)==0:
 				start = time.time()
 			# Assign the actual time to stop variable until the ECHO goes back from HIGH to LOW
-			while GPIO.input(self.gpio_echo)==1:
+			while GPIO.input(GPIO_ECHO)==1:
 				stop = time.time()
 			# Calculate the time it took the wave to travel there and back
 			measuredTime = stop - start
@@ -83,58 +72,56 @@ class ultrasonic:
 			distanceBothWays = measuredTime * 33112 # cm/s in 20 degrees Celsius
 			# Divide the distance by 2 to get the actual distance from sensor to obstacle
 			depth = distanceBothWays / 2
-			if (depth != 0):
-				depth_sum = depth_sum + depth
-				i+=1
-		average_depth = (depth_sum / i)
-		return depth
+			return depth
 
-	def check_tank(self):
-		depth_per = 100 - ((self.average_depth() / 150) * 100)
-		if (depth_per > 20):
-			#print("Reservoir d'eau rempli à {} %".format(depth_per))
-			return True
-		else:
-			return False
+		def average_depth(self):
+			i = 0
+			depth_sum = 0
+			depth = 0
+			GPIO.output(self.gpio_trigger, GPIO.LOW)
+			time.sleep(0.5)
+			while (i < 10):
+				GPIO.output(self.gpio_trigger, GPIO.HIGH) # set TRIGGER to HIGH
+				time.sleep(0.00001) # wait 10 microseconds
+				GPIO.output(self.gpio_trigger, GPIO.LOW) # set TRIGGER to LOW
+				start = time.time()
+				while GPIO.input(self.gpio_echo)==0:
+					start = time.time()
+				# Assign the actual time to stop variable until the ECHO goes back from HIGH to LOW
+				while GPIO.input(self.gpio_echo)==1:
+					stop = time.time()
+				# Calculate the time it took the wave to travel there and back
+				measuredTime = stop - start
+				# Calculate the travel distance by multiplying the measured time by speed of sound
+				distanceBothWays = measuredTime * 33112 # cm/s in 20 degrees Celsius
+				# Divide the distance by 2 to get the actual distance from sensor to obstacle
+				depth = distanceBothWays / 2
+				if (depth != 0):
+					depth_sum = depth_sum + depth
+					i+=1
+			average_depth = (depth_sum / i)
+			return depth
 
-class rain:
-	def __init__(self, gpio):
-		self.gpio = gpio
-		print('Initialisation du capteur de pluie/arrosage GPIO : {}'.format(self.gpio))
-		time.sleep(0.5)
-		GPIO.setup(self.gpio, GPIO.IN)
-	
-	def status(self):
-		status = GPIO.input(self.gpio)
-		return status	
-	
-	def check_rain(self):
-		if (self.status() == 1):
-			return True
-		else:
-			return False
+		def check_tank(self):
+			depth_per = 100 - ((self.average_depth() / 150) * 100)
+			if (depth_per > 20):
+				#print("Reservoir d'eau rempli à {} %".format(depth_per))
+				return True
+			else:
+				return False
 
-class gtc:
-	def __init__(self):
-		print('Initialisation de la Gestion Technique centralisée')
-		time.sleep(0.5)
-
-	def initialize(self):
-		GPIO.setwarnings(False)
-		GPIO.setmode(GPIO.BCM)
-		tank_min = 130
-		well_min = 250
-		watering_hour = "02:43 PM"
-		relay1 = relay(25)
-		relay2 = relay(8)
-		relay3 = relay(5)
-		relay4 = relay(6)
-		ultrasonic1 = ultrasonic(14, 15)
-		rain1 = rain(17)
-		rain2 = rain(27)
-
-	def get_depth(self):
-		return ultrasonic1.average_depth()
-
-	def cleanup(self):
-		GPIO.cleanup()
+	class rain:
+		def __init__(self, gpio):
+			self.gpio = gpio
+			print('Initialisation du capteur de pluie/arrosage GPIO : {}'.format(self.gpio))
+			time.sleep(0.5)
+			GPIO.setup(self.gpio, GPIO.IN)
+		
+		def status(self):
+			return GPIO.input(self.gpio)
+			
+		def check_rain(self):
+			if (self.status() == 1):
+				return True
+			else:
+				return False
